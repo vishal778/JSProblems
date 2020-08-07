ce992e68575ec55a344b2375d0c941646808fc01
Vishal Batra <vishal.batra@charpixel.com>
2020-08-07 19:16:40 +0530

    Books constructor completed

---
+++ Books.js
@@ -0,0 +1,23 @@
+class Books {
+    constructor(title,author) {
+        this.title = title;
+        this.author = author;
+    }
+    
+    getTitle(){
+        return "Title: " + this.title;
+    }
+    
+    getAuthor(){
+        return "Author: " + this.author;
+    }
+}
+
+var PP = new Books("Pride and Prejudice","Jane Austen");
+var H = new Books("Hamlet","William Shakespeare");
+var WP = new Books("War and Peace","Leo Tolstoy");
+
+console.log(PP.title);
+console.log(PP.author);
+console.log(PP.getTitle());
+console.log(PP.getAuthor());
--- find-range.js
+++ find-range.js
@@ -83,3 +83,5 @@ console.log(result);
 
 
 
+
+
